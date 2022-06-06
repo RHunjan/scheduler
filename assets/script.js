c799adde2e861b90c58163f2e3391f7078cdfab4
ReenaH <Reena_Hunjan@yahoo.com>
2022-06-06 19:31:52 -0400

    add current date and time to header

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,16 @@
 // Moment date and time
-var today = moment().format("YY-MM-DD");
+var today = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 var time = moment().format("HH");
 
 console.log(today);
 console.log(time);
+
+//display current date
+var dateEl = document.getElementById("currentDay");
+var currentDay = document.createElement("p");
+currentDay.textContent = today;
+dateEl.append(currentDay);
+
+//compare time
+
+
