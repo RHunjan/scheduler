659c69294ad328baa3a263a7ffb04c21a545d19f
ReenaH <Reena_Hunjan@yahoo.com>
2022-06-06 22:50:59 -0400

    added for loop and setinterval to update colors

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-var hoursArray = [09,10,11,12,13,14,15,16,17];
+var hoursArray = [9,10,11,12,13,14,15,16,17];
 
 // Moment date and time
 var today = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
@@ -12,16 +12,17 @@ $("#currentDay").text(today);
 
 //compare hour with current time
 
-var checkTime = function (){
- console.log(hoursArray[0]);
- console.log(time);
- if (time < hoursArray[0]){
-    $("#" + hoursArray[0]).addClass("future");
- } else if (time == hoursArray[0]){
-     $("#" + hoursArray[0]).addClass("present");
- } else {
-      $("#" + hoursArray[0]).addClass("past");
+var timeColor = function(){
+
+    for (var i = 0; i <hoursArray.length; i++){
+        if (time < hoursArray[i]){
+            $("#" + hoursArray[i]).addClass("future");
+      } else if (time == hoursArray[i]){
+            $("#" + hoursArray[i]).addClass("present");
+      } else {
+      $("#" + hoursArray[i]).addClass("past");
  }
+}
 };
 
-checkTime();
+setInterval(timeColor, (1000*60));
